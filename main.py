e26eb07d110776c47c35252a4d2fd2f6061a8c65
John <me@example.com>
2018-07-28 17:03:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+
+
+client = discord.Client()
+
+	
+@client.event
+async def on_member_join(member):
+	channel=client.get_channel('472729942141042688')
+    #await channel.send(msg)
+	await client.send_message(channel, 'Welcome, {}! Please read the #rules and get some #roles'.format(member.mention))
+
+
+	
+client.run('token')	
