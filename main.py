e2d13789ecdc8596aa6823af3a41287f142fba75
John <f1arenova1@gmail.com>
2018-07-29 22:37:05 +0300

    try to fix env varibles

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import discord
-
+import os
 
 client = discord.Client()
 
+token = os.environ.get('BOT_TOKEN')
+#channel_id = 
+#"NDcyNzUwMzY5ODk4NzU4MTY0.Dj38LA.BfKc1hS0xKEpUBFGfFso7WYqnDE"
 	
 @client.event
 async def on_member_join(member):
 	channel=client.get_channel('472729942141042688')
     #await channel.send(msg)
 	await client.send_message(channel, 'Welcome, {}! Please read the #rules and get some #roles'.format(member.mention))
-
+	#await self.bot.responses.success(message='Channel <#{}> will be ignored.'.format(channel.id)) 
 
 	
-client.run(token)	
+client.run(token)
