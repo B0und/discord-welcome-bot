05fe41b02d212752e64afe9b5e43ddfe78885569
John <f1arenova1@gmail.com>
2018-07-29 22:53:58 +0300

    fix

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 client = discord.Client()
 
 token = os.environ.get('BOT_TOKEN')
-#channel_id = 
-#"NDcyNzUwMzY5ODk4NzU4MTY0.Dj38LA.BfKc1hS0xKEpUBFGfFso7WYqnDE"
+channel_id = os.environ.get('channel_id')
+
 	
 @client.event
 async def on_member_join(member):
-	channel=client.get_channel('472729942141042688')
+	channel=client.get_channel(channel_id)
     #await channel.send(msg)
 	await client.send_message(channel, 'Welcome, {}! Please read the #rules and get some #roles'.format(member.mention))
 	#await self.bot.responses.success(message='Channel <#{}> will be ignored.'.format(channel.id)) 
