73187ef8d8a4303a01ef1a419564110d810f08be
John <f1arenova1@gmail.com>
2018-07-30 18:44:51 +0300

    added timer logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 
+
 client = discord.Client()
 
 token = os.environ.get('BOT_TOKEN')
@@ -14,7 +15,13 @@ async def on_member_join(member):
 	rules = client.get_channel(rules_id)
 	roles = client.get_channel(roles_id)
     #await channel.send(msg)
-	await client.send_message(channel, 'Welcome, {}! Please read the {} and get some {} !'.format(member.mention, rules.mention, roles.mention))
- 
+	def check(msg):
+		return msg.content.startswith('Welcome') and member in msg.content
+
+	message = await client.wait_for_message(timeout=10,check=check)
+	if not message:
+		await client.send_message(channel, 'Welcome, {}! Please read the {} and get some {} !'.format(member.mention, rules.mention, roles.mention))
+
+
 	
 client.run(token)
