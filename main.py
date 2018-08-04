95a218ede844b44d3cdc5e585d19c2b7b3120aef
John <me@example.com>
2018-08-04 22:28:41 +0300

    changed timer

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ token = os.environ.get('BOT_TOKEN')
 channel_id = os.environ.get('channel_id')
 roles_id = os.environ.get('roles_id')
 rules_id = os.environ.get('rules_id')
+timer = os.environ.get("timer")
 	
 @client.event
 async def on_member_join(member):
 	channel=client.get_channel(channel_id)
 	rules = client.get_channel(rules_id)
 	roles = client.get_channel(roles_id)
-    #await channel.send(msg)
+ 
 	def check(msg):
 		return msg.content.startswith('Welcome') and member.mention in msg.content
 
-	message = await client.wait_for_message(timeout=60,check=check)
+	message = await client.wait_for_message(timeout=timer,check=check)
 	if not message:
 		await client.send_message(channel, 'Welcome, {}! Please read the {} and get some {} !'.format(member.mention, rules.mention, roles.mention))
 
